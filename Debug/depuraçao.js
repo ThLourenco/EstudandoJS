597613ff0e9950086adbd7914edf0ff26f5b6130
ThLourenco <thilourenco01@gmail.com>
2026-03-21 21:38:38 -0300

    Estudando depuraçao e debug

---
+++ Debug/depuraçao.js
@@ -0,0 +1,30 @@
+"use strict";
+
+opa = "teste";
+const opa = "teste";
+
+//const undefined = 10, strick nao permite utilziar nome do java scrip para definir variaveis
+//nao pode deletar uma propriedade de uam array delete [].legth;
+
+//---------------------------------------------------------
+
+/* console.log */
+
+let a = 1;
+let b =2 2;
+
+if (a == 1){
+    a = b+2;
+}
+
+console.log(a);
+
+for (let i = 0; i<b; i++){
+    a = a +2;
+    console.log(a)
+}
+
+if(a>5){
+    a = 25;
+}
+console.log(a)
